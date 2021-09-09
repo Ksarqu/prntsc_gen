a9ddfd7cdce9f153c038eb9b83fac87c0c83981b
Ksarqu <ksarqu@gmail.com>
2021-09-09 20:37:11 +0200

    changing language to english

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from webbrowser import open
 from random import choice, randint
 from string import ascii_lowercase
 from time import sleep
+
 link = []
+
 while True:
     try:
-        repeats = int(input("Ile razy ma być wylosowany link: "))
+        repeats = int(input("How much links do you want to open? "))
         break
     except ValueError:
-        print("Wprowadź liczbę!")
+        print("Enter a number!")
 for i in range(repeats):
     a = choice(ascii_lowercase) + choice(ascii_lowercase) + str(randint(1000, 9999))
     link.append(a)
